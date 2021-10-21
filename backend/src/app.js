922cecfb0d14087985f759461422ec0716d8afe4
Carlos Roberto <carona_jr@hotmail.com>
2021-10-20 22:34:51 -0300

    add swagger docs

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import routes from './controllers'
 import db from '../config/database'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 try {
     db()
