170ec372fc290f74cbd7c43d4f7b7efa30fa6656
Carlos Roberto <carona_jr@hotmail.com>
2021-10-24 20:13:35 -0300

    disabled job

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,16 @@ import routes from './controllers'
 import db from '../config/database'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from '../swagger.json'
-import { scheduleJob } from 'node-schedule'
-import { scrapProducts } from './jobs'
-import { JOB_TIME } from '../config'
+// import { scheduleJob } from 'node-schedule'
+// import { scrapProducts } from './jobs'
+// import { JOB_TIME } from '../config'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-scheduleJob(JOB_TIME, () => scrapProducts())
+// scheduleJob(JOB_TIME, () => scrapProducts())
 
 try {
     db()
